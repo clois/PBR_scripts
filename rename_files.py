8770d344d46dce9acd657a08a101c0dc93dbd52b
Cristina Lois <crisloisg@gmail.com>
2015-09-24 13:48:41 -0400

    removed inactive lines in test_Histos_striatum.sh added script to rename files, rename_files.py

---
+++ rename_files.py
@@ -0,0 +1,17 @@
+import glob
+import shutil
+
+# Substitute path/to/dir for the directory the files are in
+#directory = '.'
+directory = '/autofs/cluster/hookerlab/Users/Cristina/fPET/Subjects_PETMR/fPET_0801_cl/PET/MR/MPRAGE'
+old_files = glob.glob(directory + '/*.dcm')
+
+# Removes four last chars in the name and add '_cris.dcm'
+new_files = [ f[:-4] + '_clois.dcm' for f in old_files ]
+
+# Builds a list of 2-tuples, where the first (second) is the old (new) file 
+zipped = zip(old_files, new_files)
+
+# Copies the old to the new files. Use 'rename' instead of 'copyfile' to move.
+for z in zipped:
+    shutil.move(z[0], z[1])
--- test_Histos_striatum.sh
+++ test_Histos_striatum.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 ##
-## TO BE USED AFTER test_ROIs_ivan_allRegions.sh ONLY!! (This version needs some files as inputs that are created running test_ROIs_ivan_allRegions.sh)
-#
+## TO BE USED AFTER 'get_PETmean_FS_ROIs.sh' or 'test_ROIs_ivan_allRegions.sh' ONLY!!!! (needs as inputs some of the files that are created running that script)
+##
 ## Provides MIN AND MAX intensity of pixels within the masked VOI, so that histo range is known. 
-## Provides HISTOGRAM values for nbins (50) between min (o.8) and max (1.8)
-#
+## Provides HISTOGRAM values for nbins (50) between min (0.8) and max (1.8)
+##
+## You NEED TO CHANGE "PET_DIR"
+
 
 ## Subject ID:
 SUBJECT_ID=$1 
@@ -23,39 +25,13 @@ mkdir -p ${PET_DIR}/Histograms
 
 cd  ${OUTPUT_DIR}
 
-process_region()
-{
-    
-    REGION=$1
-    LEFTLOWER=$2
-    LEFTUPPER=$3
-    RIGHTLOWER=$4
-    RIGHTUPPER=$5
-    IS_PATIENT=$6
 
-    #fslmaths ${OUTPUT_DIR}/aparc+aseg.nii -thr $LEFTLOWER -uthr $LEFTUPPER ${REGION}_left.nii
-    #fslmaths ${OUTPUT_DIR}/aparc+aseg.nii -thr $RIGHTLOWER -uthr $RIGHTUPPER ${REGION}_right.nii
-    
-    ## Add left and right regions to obtain bilateral values, and binarize. 
-    #fslmaths ${REGION}_left.nii.gz -add ${REGION}_right.nii.gz -bin ${REGION}_bi.nii 
-    
-    ## Reslice created file to match MPRAGE (001.mgz) dimensions --(nearest neighbour)
-    #tcsh -c "source $FREESURFER; mri_convert -rl 001.mgz -rt nearest ${REGION}_bi.nii.gz ${REGION}_bi_resliced.nii"
-    
-    ## Eliminate nan values from the image (otherwise the computation of mean and std doesn't work properly and the outputs are nan)
-    #fslmaths ${REGION}_bi_resliced.nii -nan ${REGION}_bi_mask.nii
-    
-    ## Reslice PET image to match MPRAGE dimensions --(trilinear)
-    #tcsh -c "source $FREESURFER; mri_convert -rl 001.mgz  ${OUTPUT_DIR}/${PET_file}.nii r${PET_file}.nii"
-    
-    ## Obtain mean and std of the PET image using the mask created above, and output to a txt file
-    #MEAN_AND_STD=`fslstats r${PET_file}.nii -k ${REGION}_bi_mask.nii.gz -m -s`
-    #echo ${SUBJECT_ID} ${MEAN_AND_STD} ${IS_PATIENT}>> ${PET_DIR}/ROI_Results/${REGION}_roi_values.txt
+## Obtain min and max of the PET image using the mask created the script 'get_PETmean_FS_ROIs.sh', and output to a txt file
+#    MIN_AND_MAX=`fslstats r${PET_file}.nii -k ${REGION}_bi_mask.nii.gz -R`
+#    echo ${SUBJECT_ID} ${MIN_AND_MAX} ${IS_PATIENT}>> ${PET_DIR}/Histograms/${REGION}_min_and_max_values.txt
+# }
 
-## Obtain min and max of the PET image using the mask created above, and output to a txt file
-    MIN_AND_MAX=`fslstats r${PET_file}.nii -k ${REGION}_bi_mask.nii.gz -R`
-    echo ${SUBJECT_ID} ${MIN_AND_MAX} ${IS_PATIENT}>> ${PET_DIR}/Histograms/${REGION}_min_and_max_values.txt
- }
+# Obtain histogram values for pallidum, putamen, caudate and thalamus:
   
    HISTO=`fslstats r${PET_file}.nii -k pallidum_bi_mask.nii.gz -H 50 0.8 1.8`
     echo ${SUBJECT_ID} ${HISTO} ${IS_PATIENT}>> ${PET_DIR}/Histograms/pallidum_histo_values.txt
@@ -70,12 +46,5 @@ process_region()
     echo ${SUBJECT_ID} ${HISTO4} ${IS_PATIENT}>> ${PET_DIR}/Histograms/thalamus_histo_values.txt
 
 
-
-## Process caudate, putamen, thalamus and pallidum:
-
-process_region caudate 10.9 11.1 49.9 50.1 $IS_PATIENT
-process_region putamen 11.9 12.1 50.9 51.1 $IS_PATIENT
-process_region pallidum  12.9 13.1 51.9 52.1 $IS_PATIENT
-process_region thalamus 9.9 10.1 48.9 49.1 $IS_PATIENT
 
 
