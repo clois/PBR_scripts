a63147dd09e0737272e8608328ccecb3f964c9e2
Cristina Lois <crisloisg@gmail.com>
2015-11-10 11:40:48 -0500

    Added calculation of: median, 25, and 75% to test_Histos_striatum

--- rename_files.py
+++ rename_files.py
@@ -3,11 +3,11 @@ import shutil
 
 # Substitute path/to/dir for the directory the files are in
 #directory = '.'
-directory = '/autofs/cluster/hookerlab/Users/Cristina/fPET/Subjects_PETMR/fPET_0801_cl/PET/MR/MPRAGE'
+directory = '/autofs/cluster/hookerlab/Users/Cristina/fPET/Subjects_PETMR/fPET_0901_clois/PET/MR/MPRAGE'
 old_files = glob.glob(directory + '/*.dcm')
 
 # Removes four last chars in the name and add '_cris.dcm'
-new_files = [ f[:-4] + '_clois.dcm' for f in old_files ]
+new_files = [ f[:-4] + '_cl.dcm' for f in old_files ]
 
 # Builds a list of 2-tuples, where the first (second) is the old (new) file 
 zipped = zip(old_files, new_files)
--- test_Histos_striatum.sh
+++ test_Histos_striatum.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 ##
-## TO BE USED AFTER 'get_PETmean_FS_ROIs.sh' or 'test_ROIs_ivan_allRegions.sh' ONLY!!!! (needs as inputs some of the files that are created running that script)
-##
+## TO BE USED AFTER test_ROIs_ivan_allRegions.sh ONLY!! (This version needs some files as inputs that are created running test_ROIs_ivan_allRegions.sh)
+#
 ## Provides MIN AND MAX intensity of pixels within the masked VOI, so that histo range is known. 
 ## Provides HISTOGRAM values for nbins (50) between min (0.8) and max (1.8)
-##
-## You NEED TO CHANGE "PET_DIR"
-
+#  Provides 25, 50, and 75th percentiles
 
 ## Subject ID:
 SUBJECT_ID=$1 
@@ -25,26 +23,70 @@ mkdir -p ${PET_DIR}/Histograms
 
 cd  ${OUTPUT_DIR}
 
+process_region()
+{
+    
+    REGION=$1
+    LEFTLOWER=$2
+    LEFTUPPER=$3
+    RIGHTLOWER=$4
+    RIGHTUPPER=$5
+    IS_PATIENT=$6
 
-## Obtain min and max of the PET image using the mask created the script 'get_PETmean_FS_ROIs.sh', and output to a txt file
-#    MIN_AND_MAX=`fslstats r${PET_file}.nii -k ${REGION}_bi_mask.nii.gz -R`
-#    echo ${SUBJECT_ID} ${MIN_AND_MAX} ${IS_PATIENT}>> ${PET_DIR}/Histograms/${REGION}_min_and_max_values.txt
-# }
+    #fslmaths ${OUTPUT_DIR}/aparc+aseg.nii -thr $LEFTLOWER -uthr $LEFTUPPER ${REGION}_left.nii
+    #fslmaths ${OUTPUT_DIR}/aparc+aseg.nii -thr $RIGHTLOWER -uthr $RIGHTUPPER ${REGION}_right.nii
+    
+    ## Add left and right regions to obtain bilateral values, and binarize. 
+    #fslmaths ${REGION}_left.nii.gz -add ${REGION}_right.nii.gz -bin ${REGION}_bi.nii 
+    
+    ## Reslice created file to match MPRAGE (001.mgz) dimensions --(nearest neighbour)
+    #tcsh -c "source $FREESURFER; mri_convert -rl 001.mgz -rt nearest ${REGION}_bi.nii.gz ${REGION}_bi_resliced.nii"
+    
+    ## Eliminate nan values from the image (otherwise the computation of mean and std doesn't work properly and the outputs are nan)
+    #fslmaths ${REGION}_bi_resliced.nii -nan ${REGION}_bi_mask.nii
+    
+    ## Reslice PET image to match MPRAGE dimensions --(trilinear)
+    #tcsh -c "source $FREESURFER; mri_convert -rl 001.mgz  ${OUTPUT_DIR}/${PET_file}.nii r${PET_file}.nii"
+    
+    ## Obtain mean and std of the PET image using the mask created above, and output to a txt file
+    #MEAN_AND_STD=`fslstats r${PET_file}.nii -k ${REGION}_bi_mask.nii.gz -m -s`
+    #echo ${SUBJECT_ID} ${MEAN_AND_STD} ${IS_PATIENT}>> ${PET_DIR}/ROI_Results/${REGION}_roi_values.txt
 
-# Obtain histogram values for pallidum, putamen, caudate and thalamus:
+## Obtain min and max of the PET image using the mask created above, and output to a txt file
+    MIN_AND_MAX=`fslstats r${PET_file}.nii -k ${REGION}_bi_mask.nii.gz -R`
+    echo ${SUBJECT_ID} ${MIN_AND_MAX} ${IS_PATIENT}>> ${PET_DIR}/Histograms/${REGION}_min_and_max_values_TEST.txt
+
+## Obtain the 25, 50 (median), and 75 percentile of the PET image using the mask created above, and output to a txt file
+    Percentile_25th=`fslstats r${PET_file}.nii -k ${REGION}_bi_mask.nii.gz -P 25`
+    echo ${SUBJECT_ID} ${Percentile_25th} ${IS_PATIENT}>> ${PET_DIR}/Histograms/${REGION}_25th_percentile.txt
+
+    Percentile_50th=`fslstats r${PET_file}.nii -k ${REGION}_bi_mask.nii.gz -P 50`
+    echo ${SUBJECT_ID} ${Percentile_50th} ${IS_PATIENT}>> ${PET_DIR}/Histograms/${REGION}_50th_percentile.txt
+
+    Percentile_75th=`fslstats r${PET_file}.nii -k ${REGION}_bi_mask.nii.gz -P 75`
+    echo ${SUBJECT_ID} ${Percentile_75th} ${IS_PATIENT}>> ${PET_DIR}/Histograms/${REGION}_75th_percentile.txt
+
+ }
   
-   HISTO=`fslstats r${PET_file}.nii -k pallidum_bi_mask.nii.gz -H 50 0.8 1.8`
-    echo ${SUBJECT_ID} ${HISTO} ${IS_PATIENT}>> ${PET_DIR}/Histograms/pallidum_histo_values.txt
+ #  HISTO=`fslstats r${PET_file}.nii -k pallidum_bi_mask.nii.gz -H 50 0.8 1.8`
+ #   echo ${SUBJECT_ID} ${HISTO} ${IS_PATIENT}>> ${PET_DIR}/Histograms/pallidum_histo_values.txt
 
-   HISTO2=`fslstats r${PET_file}.nii -k putamen_bi_mask.nii.gz -H 50 0.5 1.8`
-    echo ${SUBJECT_ID} ${HISTO2} ${IS_PATIENT}>> ${PET_DIR}/Histograms/putamen_histo_values.txt
+ #  HISTO2=`fslstats r${PET_file}.nii -k putamen_bi_mask.nii.gz -H 50 0.5 1.8`
+ #   echo ${SUBJECT_ID} ${HISTO2} ${IS_PATIENT}>> ${PET_DIR}/Histograms/putamen_histo_values.txt
    
-   HISTO3=`fslstats r${PET_file}.nii -k caudate_bi_mask.nii.gz -H 50 0.3 1.5`
-    echo ${SUBJECT_ID} ${HISTO3} ${IS_PATIENT}>> ${PET_DIR}/Histograms/caudate_histo_values.txt
+ #  HISTO3=`fslstats r${PET_file}.nii -k caudate_bi_mask.nii.gz -H 50 0.3 1.5`
+ #   echo ${SUBJECT_ID} ${HISTO3} ${IS_PATIENT}>> ${PET_DIR}/Histograms/caudate_histo_values.txt
 
-   HISTO4=`fslstats r${PET_file}.nii -k thalamus_bi_mask.nii.gz -H 50 0.5 1.9`
-    echo ${SUBJECT_ID} ${HISTO4} ${IS_PATIENT}>> ${PET_DIR}/Histograms/thalamus_histo_values.txt
+ #  HISTO4=`fslstats r${PET_file}.nii -k thalamus_bi_mask.nii.gz -H 50 0.5 1.9`
+ #   echo ${SUBJECT_ID} ${HISTO4} ${IS_PATIENT}>> ${PET_DIR}/Histograms/thalamus_histo_values.txt
 
 
+
+## Process caudate, putamen, thalamus and pallidum:
+
+process_region caudate 10.9 11.1 49.9 50.1 $IS_PATIENT
+process_region putamen 11.9 12.1 50.9 51.1 $IS_PATIENT
+process_region pallidum  12.9 13.1 51.9 52.1 $IS_PATIENT
+process_region thalamus 9.9 10.1 48.9 49.1 $IS_PATIENT
 
 
